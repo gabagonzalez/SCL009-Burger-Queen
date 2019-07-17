cb2d5dc6469960d5d7c4090da33066ebc5a44002
AgathaP <ebrumaktub@gmx.com>
2019-07-17 18:18:44 -0400

    Se crean componentes en una carpeta, servicio, también en su carpeta y archivos ts que contiene array de objetos con los productos a la venta

---
+++ src/app/products/food.ts
@@ -0,0 +1,23 @@
+import { Food } from './food-model';
+
+export const FOOD: Food = [
+{ id: 1.1, name: 'Cafe Americano' , price: 500},
+{ id: 1.2, name: 'Cafe con Leche' , price: 700},
+{ id: 1.3, name: 'Jugo Natural' , price: 700},
+{ id: 1.4, name: 'Agua 500ml.' , price: 500},
+{ id: 1.5, name: 'Agua 750ml.' , price: 800},
+{ id: 1.6, name: 'Gaseosa 500ml.', price: 700},
+{ id: 1.7, name: 'Bebida 750ml.' , price: 1000},
+{ id: 2.1, name: 'Sandwich de jamón y queso' , price: 1000}
+// { id: 2.2, name: '$1.500.-Magm-Hamburguesas Res Simple' , price:},
+// { id: 2.3, name: '$2.500.-Tor-Hamburguesas Res Doble' , price:},
+// { id: 2.4, name: '$1.500.-Dr N2-Hamburguesas Pollo Simple' , price:},
+// { id: 2.5, name: '$2.500.-Na2-Hamburguesas Pollo Doble' , price:},
+// { id: 2.6, name: '$1.500.-Bomb2-Hamburguesas Vegetariana Simple' , price:},
+// { id: 2.7, name: '$2.500.-Cel2-Hamburguesas Vegetariana Doble', price:},
+// { id: 3.1, name: '$500.-Mag2-Agregado/Queso' , price:},
+// { id: 3.2, name: '$500.-Rub2-Agregado/Huevo' , price:},
+// { id: 3.3, name: '$500.-Dyn2-Acomp/Papas Fritas' , price:},
+// { id: 3.4, name: '$500.-DrIQ2-Acomp/Onion Rings' , price:}
+]
+

caba0d76ae807eb5e803c15d748cf4bd362d3b5c
Relun <claudia.araya.ro@gmail.com>
2019-07-22 18:26:29 -0400

    creando función filtrar en food service

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { FOOD } from '../products/food';
+import { Food } from '../products/food-model';
 
 
 @Injectable({
   providedIn: 'root'
 })
+ 
 export class FoodService {
-
+  FOOD: Food[] = FOOD;
+  //Food:FOOD: Food[]
   constructor() {}
 
-  // getFood() {
-  //   return "array de comida";
+   getFood(type:string): Food {
 
-  // }
+    let getBreakfast = this.FOOD;
+    let filtration = getBreakfast.filter( item => item.type == type)
+    //return filtration;
+    console.log(filtration);
+    return {id: 0, type: "tipo", name: "hola", price: 3948}
+
+   }
   
 }
